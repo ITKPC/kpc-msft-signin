d97438e07cf1bf1f40562c11d7f4772b71feb1db
ITKPC <itconsult@kamloopspickleballclub.ca>
2026-07-31 14:20:09 -0700

    Configure KPC Entra application IDs

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export const entraConfig = {
-  // Microsoft Entra admin centre > App registrations > your app > Overview
-  clientId: "PASTE_APPLICATION_CLIENT_ID_HERE",
-  tenantId: "PASTE_DIRECTORY_TENANT_ID_HERE",
+  // Microsoft Entra admin centre > App registrations > KPC Sign-in Test > Overview
+  clientId: "1ea5c194-4598-4df2-aa5f-162def2dd95a",
+  tenantId: "7130f06c-6474-4dbc-bebc-ed1936c7172c",
 };
