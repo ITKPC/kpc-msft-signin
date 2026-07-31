4c542294267044c43baa7c4571279a8182349825
ITKPC <itconsult@kamloopspickleballclub.ca>
2026-07-31 14:12:08 -0700

    Add Microsoft Entra configuration placeholders

---
+++ src/config.js
@@ -0,0 +1,5 @@
+export const entraConfig = {
+  // Microsoft Entra admin centre > App registrations > your app > Overview
+  clientId: "PASTE_APPLICATION_CLIENT_ID_HERE",
+  tenantId: "PASTE_DIRECTORY_TENANT_ID_HERE",
+};
